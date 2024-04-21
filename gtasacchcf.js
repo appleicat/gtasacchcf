5ac411eb95da6de2e27ff3c5d4304fe578de8f9b
kitty <87716149+appleicat@users.noreply.github.com>
2024-04-22 03:53:50 +0400

    Update gtasacchcf.js

--- gtasacchcf.js
+++ gtasacchcf.js
@@ -1,1 +1,16 @@
 const s2h=(s)=>(~Bun.hash.crc32(s.toUpperCase().split('').reverse().join(''))>>>0).toString(16);
+const nextString = (string, alphabet) => {
+  let index = string.length - 1;
+  let base;
+  do {
+    base = string[index];
+    let stringArray = string.split('');
+    if (stringArray[index] == alphabet[alphabet.length - 1]) {
+      stringArray[index] = alphabet[0];
+      if (index == 0) stringArray.unshift(alphabet[0]);
+    } else stringArray[index] = alphabet[alphabet.indexOf(stringArray[index]) + 1];
+    string = stringArray.join('');
+    index--;
+  } while (base == alphabet[alphabet.length - 1]);
+  return string;
+};
