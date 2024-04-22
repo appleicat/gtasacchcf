f12e6a929d68bcb242f109d61444a3970af0f577
kitty <87716149+appleicat@users.noreply.github.com>
2024-04-22 23:46:56 +0400

    Update gtasacchcf.js

--- gtasacchcf.js
+++ gtasacchcf.js
@@ -1,24 +1,22 @@
 const s2h=(s)=>(~Bun.hash.crc32(s.toUpperCase().split('').reverse().join(''))>>>0).toString(16);
-const nextString = (string, alphabet) => {
-  let index = string.length - 1;
+const next = (array, alphabet) => {
+  let index = array.length - 1;
   let base;
   do {
-    base = string[index];
-    let stringArray = string.split('');
-    if (stringArray[index] == alphabet[alphabet.length - 1]) {
-      stringArray[index] = alphabet[0];
-      if (index == 0) stringArray.unshift(alphabet[0]);
-    } else stringArray[index] = alphabet[alphabet.indexOf(stringArray[index]) + 1];
-    string = stringArray.join('');
+    base = array[index];
+    if (array[index] == alphabet[alphabet.length - 1]) {
+      array[index] = alphabet[0];
+      if (index == 0) array.unshift(alphabet[0]);
+    } else array[index] = alphabet[alphabet.indexOf(array[index]) + 1];
     index--;
   } while (base == alphabet[alphabet.length - 1]);
-  return string;
+  return array;
 };
 const wordlist = (length, alphabet) => {
-  let words = [...alphabet.split('')];
+  let words = [...alphabet];
   for (let i = 1; i < length; i++)
     words = words
-      .map((word) => alphabet.split('').map((letter) => word + letter))
+      .map((word) => alphabet.map((letter) => word + letter))
       .reduce((acc, cur) => [...acc, ...cur]);
   return words;
 };
