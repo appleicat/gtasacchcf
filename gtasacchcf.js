ecff60682bfd92cad5aba2d3cfa86bb66d072740
appleicat <appleicat@outlook.com>
2025-01-02 05:44:47 +0400

    add file writer

--- gtasacchcf.js
+++ gtasacchcf.js
@@ -27,19 +27,25 @@ const wordlist = (length, alphabet) => {
 };
 const argv = Bun.argv.slice(2);
 const alphabet = argv.includes("-a")
-  ? argv[argv.indexOf("-a") + 1]
+  ? argv[argv.indexOf("-a") + 1].toUpperCase()
   : argv.includes("--alphabet")
-  ? argv[argv.indexOf("--alphabet") + 1]
+  ? argv[argv.indexOf("--alphabet") + 1].toUpperCase()
   : "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 const start = argv.includes("-s")
-  ? argv[argv.indexOf("-s") + 1]
+  ? argv[argv.indexOf("-s") + 1].toUpperCase()
   : argv.includes("--start")
-  ? argv[argv.indexOf("--start") + 1]
+  ? argv[argv.indexOf("--start") + 1].toUpperCase()
   : alphabet[0];
 const end = argv.includes("-e")
-  ? next(argv[argv.indexOf("-e") + 1].split(""), alphabet.split("")).join("")
+  ? next(
+      argv[argv.indexOf("-e") + 1].toUpperCase().split(""),
+      alphabet.split("")
+    ).join("")
   : argv.includes("--end")
-  ? next(argv[argv.indexOf("--end") + 1].split(""), alphabet.split("")).join("")
+  ? next(
+      argv[argv.indexOf("--end") + 1].toUpperCase().split(""),
+      alphabet.split("")
+    ).join("")
   : undefined;
 const hash = argv.includes("-H")
   ? argv[argv.indexOf("-H") + 1]
@@ -60,11 +66,26 @@ if (hash) {
   process.exit();
 }
 const hashes = cheathash ? [cheathash] : [...cheathashes];
+const file = Bun.file("./data/data.json");
+let data = (await file.exists()) ? JSON.parse(await file.text()) : {};
 for (
   let current = start;
   current !== end;
   current = next(current.split(""), alphabet.split("")).join("")
 ) {
   const hash = s2h(current);
-  if (hashes.includes(hash)) console.log(`[${hash}] ${current}`);
+  if (hashes.includes(hash)) {
+    if (Array.isArray(data[hash])) {
+      if (!data[hash].includes(current)) {
+        data[hash].push(current);
+        data[hash].sort();
+      }
+    } else {
+      data[hash] = [current];
+      data = Object.keys(data)
+        .sort()
+        .reduce((acc, cur) => ((acc[cur] = data[cur]), acc), {});
+    }
+    Bun.write(file, JSON.stringify(data, null, 2));
+  }
 }
